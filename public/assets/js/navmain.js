e2e289860ed6b5cd0608ea5f5cc326818db93a41
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-03-09 18:48:49 +0530

    complete Qr section without UI

--- public/assets/js/navmain.js
+++ public/assets/js/navmain.js
@@ -23,8 +23,6 @@ const navlinks = document.querySelectorAll('nav a').
 forEach(link => {
   if(link.href.includes(`${activepage}`)){
     link.classList.add('active-link');
-    console.log(link.classList);
-    console.log(link.classList.add);
   }
 });
 
