bd97fb2c1f976e8781b4cf6892d45841443d959d
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-03-10 21:54:40 +0530

    created adding links (home)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,4 @@
+// =====================================================login page=======================================================================
 (function($) {
 
 	"use strict";
@@ -23,23 +24,5 @@
 	  }
 	});
 
-})(jQuery);
-
-function handle(){
-	alert("Hello");
-}
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-
-scanner.addListener('scan', function(content) {
-	alert(content);
-});
-
-Instascan.Camera.getCameras().then(function(cameras) {
-	if (cameras.length > 0) {
-		scanner.start(cameras[0]);
-	} else {
-		console.error('No cameras found.');
-	}
-}).catch(function(e) {
-	console.error(e);
-});
+})
+// (jQuery);
