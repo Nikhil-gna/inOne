defc058e94e7f181748ee868dc679e245f415e13
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-05-02 22:50:13 +0530

    sorting only when onclick

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -454,6 +454,30 @@ onSnapshot(linkcol, (snapshot) => {
     deleteAnchor.addEventListener("click", () => {
       dropCard.remove();
     });
+
+    sortAnchor.addEventListener("click", () => {
+      const dropItems = document.getElementById('drop-items')
+
+new Sortable(dropItems, {
+    animation: 350,
+    chosenClass: "sortable-chosen",
+    dragClass: "sortable-drag",
+    store: {
+    	// We keep the order of the list
+    	set: (sortable) =>{
+    		const order = sortable.toArray()
+    		localStorage.setItem(sortable.options.group.name, order.join('|'))
+    	},
+
+    	// We get the order of the list
+    	get: (sortable) =>{
+    		const order = localStorage.getItem(sortable.options.group.name)
+    		return order ? order.split('|') : []
+			
+    	}
+	}
+}); 
+    });
     
     iconsdiv.appendChild(sortAnchor);
     iconsdiv.appendChild(deleteAnchor);
